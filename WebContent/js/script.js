cce9b63af501a247e1781c6388ae0c5af520e11a
sanket mayani <sanket mayani>
2017-03-07 14:24:04 +0530

    Merge branch 'sanket'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    bhbhbjhbjjjn

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -44,6 +44,7 @@ $(document).ready(function(){
 		reg_email_flag = true;
 		var uname = $("#reg_email").val();
 		var hreq=new XMLHttpRequest();
+		
 		hreq.onreadystatechange= function(){
 			if(hreq.readyState==4){
 				var response=hreq.responseText;
@@ -84,11 +85,11 @@ $(document).ready(function(){
 		Login Form 
 	-------------------- */
 	
-	$("#login_email").focu(function(){
+	$("#login_email").focus(function(){
 		$("#login").attr("disabled","disabled");
 	});
 	
-	$("#login_email").blu(function(){		
+	$("#login_email").blur(function(){		
 		$("#login_email_msg").hide();
 		login_email_flag = true;
 		var uname = $("#login_email").val();
@@ -112,6 +113,90 @@ $(document).ready(function(){
 	
 	$("#login_form").submit(function(){
 		return (login_email_flag);
+	});
+	
+	/* --------------------
+		Seller Registration Form 
+	-------------------- */
+	
+	
+	$("#seller_reg_email").focus(function(){
+		$("#start_selling").attr("disabled","disabled");
+	});
+	
+	$("#seller_reg_email").blur(function(){		
+		$("#seller_reg_email_msg").hide();
+		seller_reg_email_flag = true;
+		var uname = $("#seller_reg_email").val();
+		var hreq=new XMLHttpRequest();
+		hreq.onreadystatechange= function(){
+			if(hreq.readyState==4){
+				var response=hreq.responseText;
+				if(response==1){		
+					$("#seller_reg_email_msg").show();
+					seller_reg_email_flag = false;
+				}
+				else{
+					$("#start_selling").removeAttr("disabled");
+				}
+			}		
+		}
+		
+		hreq.open("get", "UserCheck?u="+uname+"&type=seller", true);
+		hreq.send();		
+	});
+	
+	$("#seller_mobile").keyup(function(){		
+		  var phoneno = /^\d{10}$/;
+		  if($("#seller_mobile").val().match(phoneno))
+		  {
+			  $("#seller_mobile_msg").hide();
+			  seller_mobile_flag = true;
+		  }
+		  else
+		  {
+		       $("#seller_mobile_msg").show();
+		       seller_mobile_flag = false;
+		  }		
+	});
+
+	$("#seller_reg_form").submit(function(){
+		return (seller_reg_email_flag && seller_mobile_flag);
+	});
+	
+	
+	/* --------------------
+		Seller Login Form 
+	-------------------- */
+	
+	$("#seller_login_email, #seller_login_email2").focus(function(){
+		$("#seller_login, #seller_login2").attr("disabled","disabled");
+	});
+	
+	$("#seller_login_email, #seller_login_email2").blur(function(){		
+		$("#seller_login_email_msg, #seller_login_email_msg2").hide();
+		seller_login_email_flag = true;
+		var uname = $(this).val();
+		var hreq=new XMLHttpRequest();
+		hreq.onreadystatechange= function(){
+			if(hreq.readyState==4){
+				var response=hreq.responseText;
+				if(response==0){		
+					$("#seller_login_email_msg, #seller_login_email_msg2").show();
+					seller_login_email_flag = false;
+				}
+				else{
+					$("#seller_login, #seller_login2").removeAttr("disabled");
+				}
+			}		
+		}
+		
+		hreq.open("get", "UserCheck?u="+uname+"&type=seller", true);
+		hreq.send();		
+	});
+	
+	$("#seller_login_form, #seller_login_form2").submit(function(){
+		return (seller_login_email_flag);
 	});
 	
 });
