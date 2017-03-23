271484fd4bee695d1ab09c75a4fc776ff709bfaf
sanket-mayani <srmayani@gmail.com>
2017-03-23 20:41:28 +0530

    order can not be placed if cart is empty or user has logged out

--- src/controller/PlaceOrder.java
+++ src/controller/PlaceOrder.java
@@ -26,123 +26,97 @@ import java.util.Timer;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-
-/**
- * Servlet implementation class PlaceOrder
- */
 @WebServlet("/PlaceOrder")
 public class PlaceOrder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public PlaceOrder() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
 		HttpSession session = request.getSession();
 		
-		Cart cart = null; User user = null;
+		Cart cart = null;
+		User user = null;
 		
-		if(session.getAttribute("cart") != null)
+		if(session.getAttribute("cart")!=null && ((Cart)session.getAttribute("cart")).getCount()>0)
+		{	
 			cart = (Cart)session.getAttribute("cart");
-		else
-			session.setAttribute("cart", cart);
-
-		if(session.getAttribute("user") != null)
-			user = (User)session.getAttribute("user");
 		
-		PrintWriter out = response.getWriter();
+			if(session.getAttribute("user") != null)
+			{	
+				user = (User)session.getAttribute("user");
 		
-		String fn =request.getParameter("fn");
-		
-		String ln =request.getParameter("ln");
-		
-		String email = request.getParameter("email");
-		
-		long mobile = Long.parseLong(request.getParameter("mobile"));
-		
-		String address = request.getParameter("address");
-		
-		int pin = Integer.parseInt(request.getParameter("pin"));
-		
-		String city = request.getParameter("city");
-		
-		
-		
-		ArrayList<Item> items =cart.getItems();
-		
-		out.println("items in the list are :" + items.size());
-		
-		out.println("\n");
-		
-		for(int i=0; i < items.size(); i++)
-		{
-			Product product =items.get(i).getProduct();
-			Seller seller =items.get(i).getProduct().getSeller();
-			
-			int quantity = items.get(i).getQuantity();
-			
-			out.println("quantity of this item is : " + quantity);
-			
-			float amount =(items.get(i).getProduct().getPrice()) * quantity;
-			
-			DAO dao = new DAO();
-			
-	        Order order = new Order();
-	        out.println("adding records into db now into order table \n");
-	     // ADDING customer personal info into db      
-	        	order.setFname(fn);
-	        	order.setLname(ln);
-	        	order.setEmail(email);
-	        	order.setMobile(mobile);
-	        	order.setPincode(pin);
-	        	order.setAddress(address);
-	        
-	     // adding date and time into database   	
-	        	
-	        	Date now = new Date();       // current date and time
-	    // date object is created which gvs current date and time
-		// date format in mysql will be "yyyy:mm:dd" 
-	        	
-	        //	order.setDate(now);
-	        //	order.setTime(now);
-	    
-	        	order.setDateTime(now);        // this will add timestamp : date n time togehter
-	        	
-	    // adding pid and sid for this item we ve fetched abv
-	        	
-	        	order.setUser(user);
-	        	order.setSeller(seller);
-	        	order.setProduct(product);
-	        	
-			String status ="placed";
-			
-			order.setStatus(status);
-			
-		// adding quantity and amnt 
-			
-			order.setQuantity(quantity);
-			order.setAmount(amount);
-			
-			dao.insertOrder(order);      // method need to save everything in DB
+				String fn =request.getParameter("fn");
+				String ln =request.getParameter("ln");
+				String email = request.getParameter("email");
+				long mobile = Long.parseLong(request.getParameter("mobile"));
+				String address = request.getParameter("address");
+				int pin = Integer.parseInt(request.getParameter("pin"));
+				
+				ArrayList<Item> items =cart.getItems();
+				
+				for(int i=0; i < items.size(); i++)
+				{
+					Product product =items.get(i).getProduct();
+					Seller seller =items.get(i).getProduct().getSeller();
+					
+					int quantity = items.get(i).getQuantity();
+					
+					float amount =(items.get(i).getProduct().getPrice()) * quantity;
+					
+					DAO dao = new DAO();
+					
+			        Order order = new Order();
+			        
+			     // ADDING customer personal info into db      
+			        	order.setFname(fn);
+			        	order.setLname(ln);
+			        	order.setEmail(email);
+			        	order.setMobile(mobile);
+			        	order.setPincode(pin);
+			        	order.setAddress(address);
+			        
+			     // adding date and time into database   	
+			        	
+			        	Date now = new Date();       // current date and time
+			   
+			        	order.setDateTime(now);        // this will add timestamp : date n time togehter
+			        	
+			    // adding pid and sid for this item we ve fetched abv
+			        	
+			        	order.setUser(user);
+			        	order.setSeller(seller);
+			        	order.setProduct(product);
+			        	
+					String status ="placed";
+					
+					order.setStatus(status);
+					
+				// adding quantity and amnt 
+					
+					order.setQuantity(quantity);
+					order.setAmount(amount);
+					
+					dao.insertOrder(order);      // method need to save everything in DB
+				}
+			}
+			else
+				response.sendRedirect(request.getHeader("referer"));
 		}
-		
+		else
+		{
+			session.setAttribute("message", "Cart is Empty");
+			session.setAttribute("class", "alert-danger");
+			response.sendRedirect("index.jsp");
+		}
 		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
--- src/model/Order.java
+++ src/model/Order.java
@@ -1,18 +1,8 @@
-//TemporalType.DATE: maps the date as java.sql.Date.
-//TemporalType.TIME: maps the date as java.sql.Time.
-//TemporalType.TIMESTAMP: maps the date as java.sql.Timestamp.
-
-
-
 
 package model;
 
-import javax.persistence.*;
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.util.Date;
 
-@Entity
 public class Order {
 	
 	private int oid;
@@ -25,31 +15,13 @@ public class Order {
 	private String address;
 	private int pincode;
 	private String Status;
-	//private String date;
-	//private String time;
 	
 	private User user;
 	private Product product;
 	private Seller seller;
 	
-	// for date and time
-	
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Temporal(TemporalType.DATE)
-    private Date date;
-    @Temporal(TemporalType.TIME)
-    private Date time;
-    @Temporal(TemporalType.TIMESTAMP)
     private Date dateTime;
 
-    //	
-	
-	
-	
-	
 	public String getFname() {
 		return fname;
 	}
@@ -116,24 +88,7 @@ public class Order {
 	}
 	public void setStatus(String status) {
 		Status = status;
-	}
-
-/*	
-	
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public String getTime() {
-		return time;
-	}
-	public void setTime(String time) {
-		this.time = time;
-	}
-	
-*/	
+	}	
 	
 	public User getUser() {
 		return user;
@@ -147,41 +102,13 @@ public class Order {
 	public void setProduct(Product product) {
 		this.product = product;
 	}
-	
-	// for date and time
-	
-	  public Date getDate() {
-	        return date;
-	    }
 
-	    public void setDate(Date date) {
-	        this.date = date;
-	    }
+	public Date getDateTime() {
+	      return dateTime;
+	}
 
-	    public Date getTime() {
-	        return time;
-	    }
-
-	    public void setTime(Date time) {
-	        this.time = time;
-	    }
-
-	    public Date getDateTime() {
-	        return dateTime;
-	    }
-
-	    public void setDateTime(Date dateTime) {
-	        this.dateTime = dateTime;
-	    }
-
-	    @Override
-	    public String toString() {
-	        return "DateAndTime{" +
-	                "id=" + id +
-	                ", date=" + date +
-	                ", time=" + time +
-	                ", dateTime=" + dateTime +
-	                '}';
-	    }
+	public void setDateTime(Date dateTime) {
+	      this.dateTime = dateTime;
+	}
 	
 }
