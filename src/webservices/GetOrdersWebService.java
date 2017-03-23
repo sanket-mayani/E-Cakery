66d32fbc8b0f4dc5297b0f06c6c0fe4a8fd113a4
mehtavinit9896 <mehtavinit9896@gmail.com>
2017-03-23 14:23:55 +0530

    webservice is working

--- src/webservices/GetOrdersWebService.java
+++ src/webservices/GetOrdersWebService.java
@@ -13,13 +13,13 @@ import javax.ws.rs.core.MediaType;
 import model.DAO;
 import model.Order;
 
-@Path("/getorders")  
+@Path("/webservices")  
 public class GetOrdersWebService {  
   // This method is called if HTML and XML is not requested  
 	DAO dao=new DAO();
 	
 		@GET
-		@Path("/order1")
+		@Path("/getpackedorders")
 		@Produces(MediaType.APPLICATION_JSON)
 		public List<Order> orders() {
 			return dao.getPackedOrders();
